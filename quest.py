f1e5f403148fb02e70a5ed4ab049f18d069df2f6
Markokakola <148535019+markokakola@users.noreply.github.com>
2023-11-07 22:28:04 +0500

    Update quest.py

--- quest.py
+++ quest.py
@@ -17,44 +17,42 @@ YELLOW = (255, 255, 0)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("2D Quest Game")
 
-player = pygame.Rect(100, 100, PLAYER_SIZE, PLAYER_SIZE)
+player = pygame.Rect(40, 520, PLAYER_SIZE, PLAYER_SIZE)
 player_color = WHITE
 player_speed = 1
 
 # Define a chest and a key
-chest = pygame.Rect(600, 100, PLAYER_SIZE, PLAYER_SIZE)
+chest = pygame.Rect(150, 530, PLAYER_SIZE, PLAYER_SIZE)
 chest_locked = True
 
 # Define the key color and draw it
-key = pygame.Rect(200, 100, 30, 15)
+key = pygame.Rect(250, 400, 30, 15)
 key_color = (200, 150, 0)
 key_picked_up = False
 
-obstacles= [pygame.Rect(200, 200, 100, 20), 
-            pygame.Rect(600, 400, 20, 150),
-            pygame.Rect(350, 250, 100, 20),
-            pygame.Rect(450, 350, 20, 100),
-            pygame.Rect(100, 450, 200, 20),
-            pygame.Rect(400, 200, 100, 20),
-            pygame.Rect(200, 400, 20, 100),
-            pygame.Rect(550, 450, 200, 20),
-            pygame.Rect(650, 250, 20, 100),
-            pygame.Rect(50, 50, 20, 100),
-            pygame.Rect(100, 200, 20, 100),
-            pygame.Rect(300, 100, 100, 20),
-            pygame.Rect(400, 300, 20, 100),
-            pygame.Rect(550, 150, 20, 100),
-            pygame.Rect(700, 500, 100, 20),
-            pygame.Rect(500, 250, 100, 20),
-            pygame.Rect(250, 400, 20, 100),
-            pygame.Rect(650, 450, 200, 20),
-            pygame.Rect(500, 100, 100, 20),
-            pygame.Rect(450, 200, 20, 100),
-            pygame.Rect(200, 250, 100, 20),
-            pygame.Rect(450, 450, 20, 100),
-            pygame.Rect(350, 150, 100, 20),
-            pygame.Rect(600, 250, 20, 100),
-            pygame.Rect(250, 500, 100, 20),]
+obstacles = [pygame.Rect(100, 100, 20, 600),
+    pygame.Rect(300, 100, 400, 20),
+    pygame.Rect(100, 500, 400, 20),
+    pygame.Rect(680, 100, 20, 420),
+    pygame.Rect(180, 100, 20, 300),
+    pygame.Rect(500, 100, 20, 100),
+    pygame.Rect(500, 400, 100, 20),
+    pygame.Rect(600, 400, 20, 100),
+    pygame.Rect(600, 200, 100, 20),
+    pygame.Rect(200, 100, 20, 300),
+    pygame.Rect(200, 200, 100, 20),
+    pygame.Rect(180, 10, 40, 100),
+    pygame.Rect(600, 500, 100, 20),
+    pygame.Rect(500, 500, 100, 20),
+    pygame.Rect(350, 250, 20, 100),
+    pygame.Rect(200, 350, 200, 30),
+    pygame.Rect(350, 300, 20, 100),
+    pygame.Rect(350, 100, 20, 100),
+    pygame.Rect(200, 450, 200, 50),
+    pygame.Rect(0, 0, SCREEN_WIDTH, 20),
+    pygame.Rect(0, SCREEN_HEIGHT - 20, SCREEN_WIDTH, 20),
+    pygame.Rect(0, 0, 20, SCREEN_HEIGHT),
+    pygame.Rect(SCREEN_WIDTH - 20, 0, 20, SCREEN_HEIGHT),]
 surroundings = [pygame.Rect(0, 0, 800, 800)]
 
 quests = [{"description": "Open chest", "completed": False}]
@@ -68,14 +66,36 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
+
+    # Calculate the new position based on player's input
+    new_x = player.x
+    new_y = player.y
+
     if keys[pygame.K_LEFT]:
-        player.x -= player_speed
+        new_x -= player_speed
     if keys[pygame.K_RIGHT]:
-        player.x += player_speed
+        new_x += player_speed
     if keys[pygame.K_UP]:
-        player.y -= player_speed
+        new_y -= player_speed
     if keys[pygame.K_DOWN]:
-        player.y += player_speed
+        new_y += player_speed
+
+    # Create a new Rect representing the player's potential new position
+    new_player_rect = pygame.Rect(new_x, new_y, PLAYER_SIZE, PLAYER_SIZE)
+
+    # Check for collisions with obstacles
+    collision = False
+    for obstacle in obstacles:
+        if new_player_rect.colliderect(obstacle):
+            collision = True
+            break
+
+    # Only update the player's position if there's no collision
+    if not collision:
+        player.x = new_x
+        player.y = new_y
+
+    screen.fill((0, 0, 0))
 
     # Check for collisions with obstacles
     for obstacle in obstacles:
